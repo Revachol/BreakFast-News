2fec21e332463103c4be3c4cf0335dfb8cba9e9c
Revachol <egorov.dima1470@gmail.com>
2024-10-27 12:44:57 +0300

    fix crawler

--- app/users/schema.py
+++ app/users/schema.py
@@ -4,6 +4,9 @@
 
 from pydantic import BaseModel
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 class UserBase(BaseModel):
     username: str
--- app/users/view.py
+++ app/users/view.py
@@ -1,15 +1,14 @@
-# app/views/user_view.py
-from fastapi import APIRouter, Depends, HTTPException, status, Response
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.users.auth import create_access_token, verify_password
 from app.config import settings
+from jose import JWTError, jwt
 
-from jose import JWTError, jwt  # Импортируем jwt из jose
-
-from app.users.schema import UserResponse, UserCreate
+from app.users.schema import UserResponse, UserCreate, Token
 from app.users.controller import create_user, get_user, get_user_by_name
 from app.exceptions import IncorrectEmailOrPassword, UserAlreadyExistsException
+from app.users.auth import oauth2_scheme
 
 router = APIRouter()
 
@@ -21,9 +20,18 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
         raise UserAlreadyExistsException
     return create_user(db, user)
 
+@router.post("/login", response_model=Token)
+def login(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_name(db, user.username)
 
-"""
-@router.get("/users/me")
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    access_token = create_access_token(data={"sub": db_user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/users/me", response_model=UserResponse)
 def read_users_me(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -45,4 +53,3 @@ def read_users_me(token: str = Depends(oauth2_scheme), db: Session = Depends(get
     if user is None:
         raise credentials_exception
     return user
-"""
